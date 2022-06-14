bdac63a809521fd673d2963d1315b26d5dacc5bd
islambelhardi <86858341+islambelhardi@users.noreply.github.com>
2022-06-14 12:48:04 +0100

    added home page

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,82 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+import '../components/coffee_card.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+          leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.person))],
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+            BottomNavigationBarItem(icon: Icon(Icons.favorite), label: ''),
+            BottomNavigationBarItem(icon: Icon(Icons.notifications), label: ''),
+          ],
+        ),
+        body: Column(
+          children: [
+            // best coffee for you
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Text(
+                'Find the best coffee for you',
+                style: TextStyle(
+                  fontSize: 40,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            // search bar
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: TextField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search),
+                  hintText: 'Find your coffee ...',
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey.shade600),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey.shade600),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            // horiwontal coffee list
+            Expanded(
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  CoffeeCard()
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
