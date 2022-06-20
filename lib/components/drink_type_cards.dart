610ee710e3823a7ac171aa45a037f187b91326e6
GAMA <meraghniakram6@gmail.com>
2022-06-20 03:15:35 +0100

    added types of drinks

---
+++ lib/components/drink_type_cards.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class Drink_type extends StatelessWidget {
+  final String drink_type;
+  final bool isSelected;
+  final VoidCallback onTap;
+
+  Drink_type(
+      {required this.drink_type,
+      required this.isSelected,
+      required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    double height = size.height;
+    double width = size.width;
+    // TODO: implement build
+    return InkWell(
+      borderRadius: BorderRadius.circular(15),
+      onTap: onTap,
+      child: Container(
+          margin: EdgeInsets.only(
+            left: width * 0.07,
+            right: width * 0.07,
+            top: height * 0.006,
+          ),
+          child: Stack(
+            alignment: Alignment.topCenter,
+            children: [
+              Positioned(
+                child: Text(
+                  drink_type,
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: isSelected ? Colors.orange : Colors.white),
+                ),
+              ),
+              Positioned(
+                top: -15,
+                child: Text(
+                  '.',
+                  style: TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                      color: isSelected ? Colors.orange : Colors.white),
+                ),
+              ),
+            ],
+          )),
+    );
+  }
+}
