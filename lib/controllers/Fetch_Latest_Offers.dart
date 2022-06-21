6f67d77945e41157a67d9f7f102df972b306c28e
GAMA <meraghniakram6@gmail.com>
2022-06-21 01:09:01 +0100

    added assets

--- lib/controllers/Fetch_Latest_Offers.dart
+++ lib/controllers/Fetch_Latest_Offers.dart
@@ -12,10 +12,10 @@ class Fetch_Latest_Offers extends GetxController{
   void FetchData()async{
     await Future.delayed(Duration(seconds: 2),);
     var productResult =[
-      Product(1, 'coffee', 'productmage', 'productImage', 3),
-      Product(2, 'tea', 'producImage', 'prage', 3),
-      Product(3, 'turkish tea', 'productImage', 'producImage', 30),
-      Product(4, 'morrocan tea', 'productIme', 'productImage', 30),
+      Product(1, 'Expresso', 'assets/images/expresso.jpg', 'productImage', 3),
+      Product(2, 'Cappuccino', 'assets/images/coffee.jpg', 'prage', 3),
+      Product(3, 'Cremy Coffee', 'assets/images/creme_coffee.jpg', 'producImage', 30),
+      Product(4, 'morrocan tea', 'assets/images/original_black_tea.jpg', 'productImage', 30),
     ];
     productList.assignAll(productResult);
   }
