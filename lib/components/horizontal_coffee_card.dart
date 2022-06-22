3219f63545d7e89020df7c7115162274296cb7b0
GAMA <meraghniakram6@gmail.com>
2022-06-22 16:09:09 +0100

    fixed height

--- lib/components/horizontal_coffee_card.dart
+++ lib/components/horizontal_coffee_card.dart
@@ -19,88 +19,91 @@ class _HorizontalCoffeeCardState extends State<HorizontalCoffeeCard> {
     Size size = MediaQuery.of(context).size;
     double height = size.height;
     double width = size.width;
-    return Padding(
-      padding: EdgeInsets.only(left: width * 0.06),
-      child: Container(
-        padding: const EdgeInsets.all(18),
-        width: width * 0.55,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color(0xFF262a33).withOpacity(1),
-              Color(0xFF262a33).withOpacity(0.8),
-              Color(0xFF262a33).withOpacity(0.4),
-              Color(0xFF262a33).withOpacity(0.2),
-              Color(0xFF262a33).withOpacity(0.1),
-              Color(0xFF262a33).withOpacity(0.05),
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
+    return SingleChildScrollView(
+      physics: NeverScrollableScrollPhysics(),
+      child: Padding(
+        padding: EdgeInsets.only(left: width * 0.06),
+        child: Container(
+          padding: const EdgeInsets.all(18),
+          width: width * 0.55,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xFF262a33).withOpacity(1),
+                Color(0xFF262a33).withOpacity(0.8),
+                Color(0xFF262a33).withOpacity(0.4),
+                Color(0xFF262a33).withOpacity(0.2),
+                Color(0xFF262a33).withOpacity(0.1),
+                Color(0xFF262a33).withOpacity(0.05),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.circular(30),
           ),
-          borderRadius: BorderRadius.circular(30),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              height: height*0.2,
-              width: width*0.5,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Image.asset(
-                  widget.productImage,
-                  fit: BoxFit.cover,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: height*0.2,
+                width: width*0.5,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset(
+                    widget.productImage,
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: height*0.01,
-            ),
-            Text(widget.productName,style: Style),
-             SizedBox(
-               height: height*0.001,
-            ),
-            Text(
-              widget.productDescription,
-              style: TextStyle(color: Colors.grey[500]),
-            ),
-            SizedBox(
-              height: height*0.001,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                   Text.rich(
-                    TextSpan(
-                      text: '\$ ',
-                      style:
-                          TextStyle(color: Colors.orange),
-                      children: <InlineSpan>[
-                        TextSpan(
-                          text: widget.price.toString(),
-                          style: TextStyle(
-                              fontSize: 13,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white),
-                        )
-                      ],
-                    ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(6),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.orange),
-                    child: const Icon(
-                      Icons.add,
-                    ),
-                  ),
-                ],
+              SizedBox(
+                height: height*0.01,
               ),
-            ),
-          ],
+              Text(widget.productName,style: Style),
+               SizedBox(
+                 height: height*0.001,
+              ),
+              Text(
+                widget.productDescription,
+                style: TextStyle(color: Colors.grey[500]),
+              ),
+              SizedBox(
+                height: height*0.001,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                     Text.rich(
+                      TextSpan(
+                        text: '\$ ',
+                        style:
+                            TextStyle(color: Colors.orange),
+                        children: <InlineSpan>[
+                          TextSpan(
+                            text: widget.price.toString(),
+                            style: TextStyle(
+                                fontSize: 13,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(6),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.orange),
+                      child: const Icon(
+                        Icons.add,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/components/vertical_coffee_cards.dart
+++ lib/components/vertical_coffee_cards.dart
@@ -25,120 +25,123 @@ class _VerticalCoffeeCardState extends State<VerticalCoffeeCard> {
     Size size = MediaQuery.of(context).size;
     double height = size.height;
     double width = size.width;
-    return Padding(
-      padding: EdgeInsets.only(left: width * 0.06, right: width * 0.06),
-      child: Container(
-        margin: EdgeInsets.only(bottom: height * 0.01),
-        padding: const EdgeInsets.all(18),
-        height: height * 0.2,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color(0xFF262a33).withOpacity(0.8),
-              Color(0xFF262a33).withOpacity(0.6),
-              Color(0xFF262a33).withOpacity(0.4),
-              Color(0xFF262a33).withOpacity(0.4),
-              Color(0xFF262a33).withOpacity(0.1),
-              Color(0xFF262a33).withOpacity(0.05),
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomCenter,
+    return SingleChildScrollView(
+      physics: NeverScrollableScrollPhysics(),
+      child: Padding(
+        padding: EdgeInsets.only(left: width * 0.06, right: width * 0.06),
+        child: Container(
+          margin: EdgeInsets.only(bottom: height * 0.01),
+          padding: const EdgeInsets.all(18),
+          height: height * 0.2,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xFF262a33).withOpacity(0.8),
+                Color(0xFF262a33).withOpacity(0.6),
+                Color(0xFF262a33).withOpacity(0.4),
+                Color(0xFF262a33).withOpacity(0.4),
+                Color(0xFF262a33).withOpacity(0.1),
+                Color(0xFF262a33).withOpacity(0.05),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomCenter,
+            ),
+            borderRadius: BorderRadius.circular(20),
           ),
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Flexible(
-              flex: 1,
-              child: Container(
-                height: height * 0.25,
-                width: width * 0.25,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(12),
-                  child: Image.asset(
-                    'assets/images/coffee.jpg',
-                    scale: 4,
-                    fit: BoxFit.cover,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Flexible(
+                flex: 1,
+                child: Container(
+                  height: height * 0.25,
+                  width: width * 0.25,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: Image.asset(
+                      'assets/images/coffee.jpg',
+                      scale: 4,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              width: width * 0.02,
-            ),
-            Flexible(
-              flex: 2,
-              child: Container(
-                // color: Colors.deepPurpleAccent,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(left: width * 0.02),
-                      child: Text(widget.productName),
-                    ),
-                    SizedBox(
-                      height: height * 0.01,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(left: width * 0.02),
-                      child: Text(
-                        widget.productDescription,
-                        style: TextStyle(color: Colors.grey[500]),
+              SizedBox(
+                width: width * 0.02,
+              ),
+              Flexible(
+                flex: 2,
+                child: Container(
+                  // color: Colors.deepPurpleAccent,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: width * 0.02),
+                        child: Text(widget.productName),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text.rich(
-                            TextSpan(
-                              text: '\$ ',
-                              style: TextStyle(
-                                  color: Color.fromARGB(255, 209, 119, 66)),
-                              children: <InlineSpan>[
-                                TextSpan(
-                                  text: widget.price.toString(),
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white),
-                                )
-                              ],
-                            ),
-                          ),
-                          InkWell(
-                            splashColor: Colors.lightGreenAccent,
-                            onTap: () {
-                              add_to_wishlist_controller.addToWishlist(Product(
-                                  widget.id,
-                                  widget.productName,
-                                  widget.productImage,
-                                  widget.productDescription,
-                                  widget.price));
-                            },
-                            child: Container(
-                              padding: const EdgeInsets.all(8),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                color: Colors.orange,
-                              ),
-                              child: Icon(
-                                Icons.add,
+                      SizedBox(
+                        height: height * 0.01,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: width * 0.02),
+                        child: Text(
+                          widget.productDescription,
+                          style: TextStyle(color: Colors.grey[500]),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text.rich(
+                              TextSpan(
+                                text: '\$ ',
+                                style: TextStyle(
+                                    color: Color.fromARGB(255, 209, 119, 66)),
+                                children: <InlineSpan>[
+                                  TextSpan(
+                                    text: widget.price.toString(),
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white),
+                                  )
+                                ],
                               ),
                             ),
-                          ),
-                        ],
+                            InkWell(
+                              splashColor: Colors.lightGreenAccent,
+                              onTap: () {
+                                add_to_wishlist_controller.addToWishlist(Product(
+                                    widget.id,
+                                    widget.productName,
+                                    widget.productImage,
+                                    widget.productDescription,
+                                    widget.price));
+                              },
+                              child: Container(
+                                padding: const EdgeInsets.all(8),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Colors.orange,
+                                ),
+                                child: Icon(
+                                  Icons.add,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
